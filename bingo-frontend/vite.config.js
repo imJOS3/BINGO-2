5e111567e65f695cd200e06791c4801e2402ccc5
jose <josebenjumea2005@gmail.com>
2024-11-20 21:36:57 -0500

    changes jose is imbel

--- bingo-frontend/vite.config.js
+++ bingo-frontend/vite.config.js
@@ -1,23 +1,7 @@
-import { defineConfig } from 'vite';
-import preact from '@preact/preset-vite';
-import jsx from '@vitejs/plugin-vue-jsx';
-import dotenv from 'dotenv';
+import { defineConfig } from 'vite'
+import preact from '@preact/preset-vite'
 
-// Carga las variables de entorno
-dotenv.config();
-
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [preact(), jsx()],
-  server: {
-    proxy: {
-      '/socket.io': {
-        target: process.env.VITE_API_URL, 
-        ws: true,
-      },
-    },
-  },
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.js$/,
-  },
-});
+  plugins: [preact()],
+})
